a2b017208ca2f7965256899c41696de6f4b8d5f3
ks563 <karen.shea90@gmail.com>
2019-01-28 20:57:21 -0600

    smoothed spinning and hides record on submit button

--- assets/javascript/animate.js
+++ assets/javascript/animate.js
@@ -4,17 +4,13 @@ var spin = anime({
     targets:  el,
     rotate: {
         value: 360,
-        duration: 1800,
-        easing: "easeInOutSine"
+        duration: 2500,
+        easing: "linear"
       },
     loop: true,
 });
 
 
-// el.addEventListener('change', function() {  
-//   // console.log(value);
-//   // console.log(pos);
-//   var value = this.value;
-//   anime.speed = value/20;
-//   // console.log(block);
-// })
+$("#submit").click(function(){
+    $("#record-container").hide();
+  });
